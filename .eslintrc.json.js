3ec377d05457a63da1431f03637c8e86f7f00e8e
Diego Rodrigues de Brito <diego.brittto@outlook.com>
2025-06-04 09:26:56 -0300

    atualizando o codigo

---
+++ .eslintrc.json.js
@@ -0,0 +1,18 @@
+// ESLint configuration file
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-unused-vars": "warn",
+    "no-console": "off"
+  }
+};
